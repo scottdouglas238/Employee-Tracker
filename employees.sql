0288caa027c5ba22c081119f37a2d2319dd98c13
Scott <scottdouglas238@gmail.com>
2020-09-05 22:45:05 -0700

    Basic Prompts in Place

--- employees.sql
+++ employees.sql
@@ -1,13 +1,27 @@
-CREATE database employees;
+DROP DATABASE IF EXISTS employees;
+CREATE DATABASE employees;
 
 USE employees;
 
-CREATE TABLE employees (
-   id INT NOT NULL,
-   first_name VARCHAR(30),
-   last_name VARCHAR(30),
-   title VARCHAR(30),
-   department VARCHAR(30),
-   salary INT NOT NULL,
-   manager VARCHAR(30),
+CREATE TABLE department(
+  id INT NOT NULL AUTO_INCREMENT,
+  departmentName VARCHAR(30) DEFAULT '' NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role(
+  id INT NOT NULL AUTO_INCREMENT, 
+  title VARCHAR(30),
+  salary INT default 0,
+  department_id INT NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee(
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id VARCHAR(30),
+  manager_id VARCHAR(30) DEFAULT '' NOT NULL,
+  PRIMARY KEY (id)
 );
--- server.js
+++ server.js
@@ -1,13 +1,124 @@
-const express = require("express");
-const logger = require("morgan");
-const fs = require("require");
-const inquire = require("inquire");
+const mysql = require("mysql");
+const inquirer = require("inquirer");
 
-const app = express();
-const PORT = 8080;
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "employees"
+  });
 
-app.use(express.urlencoded({extended: true }));//this is the middleware
-app.use(express.json());
-app.use(logger("dev"));
-app.use(express.static("public"));//fixes everything!
+  connection.connect(function(err) {
+    if (err) throw err;
+    start();
+  });
 
+const start = () => {
+    inquirer.prompt([
+      {
+        name: "prompt",
+        type: "list",
+        message: "What would you like to do?",
+        choices: ["View all employees", "View all departments", "View all roles", "Add employee", 
+        "Remove employee", "Add department", "Remove department", "Add role", "Remove role"],
+      }
+    ])
+    .then(function(response){
+    if (response.prompt === "View all employees"){
+      viewAllEmployees();
+      start();
+    }else if(response.prompt === "View all departments"){
+      viewDepartments();
+      start();
+    }else if(response.prompt === "View all roles"){
+      viewRoles();
+      start();
+    }else if(response.prompt === "Add employee"){
+      addemployeePrompt();
+    }else if(response.prompt === "Remove employee"){
+      removeEmployee();
+      start();
+    }else if(response.prompt === "Add department"){
+      addDepartment();
+      start();
+    }else if(response.prompt === "Remove department"){
+      removeDepartment();
+      start();
+    }else if(response.prompt === "Add role"){
+      addRole();
+      start();
+    }else if(response.prompt === "Remove role"){
+      removeRole();
+      start();
+    }
+    });
+
+};
+const addemployeePrompt = () => {
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "firstName",
+        message: "What is the employee's first name?"
+      },
+      {
+        type: "input",
+        name: "lastName",
+        message: "What is the employee's last name?"
+      },
+      {
+        type: "list",
+        name: "role",
+        message: "What is the employee's role?",
+        choices: ["Sales Lead", "Salesperson", "Software Engineer", "Account Manager", "Accountant"]
+      },
+      {
+        type: "list",
+        name: "manager",
+        message: "Who is the employee's manager?",
+        choices: ["Matt", "Mindy", "Sarah", "Christina"]
+      }
+    ]).then(function(response){
+      console.log(response)
+      connection.query(
+        "INSERT INTO employee SET ?",
+        {
+          first_name: response.firstName,
+          last_name: response.lastName,
+          // title: response.role,
+          // manager: response.manager,
+        },
+        function(err){
+          if (err) throw err;
+          console.log("You updated employees.")
+          start();
+        }
+      )
+    })
+};
+
+const viewAllEmployees = () => {
+   connection.query("SELECT * FROM employee", function(err, res){
+     if (err) throw err;
+     console.log("------------------------------------------------------");
+     console.table(res);
+   }
+   )
+};
+
+const viewDepartments = () => {
+  connection.query("SELECT * FROM department", function(err, res){
+    if(err) throw err;
+    console.log("-------------------------------------------------------");
+    console.table(res);
+  })
+}
+
+const viewRoles = () => {
+  connection.query("SELECT * FROM role", function(err, res){
+    if(err) throw err;
+    console.log("-------------------------------------------------------");
+    console.table(res);
+  })
+}
