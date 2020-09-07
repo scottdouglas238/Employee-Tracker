7bec65562ba90203ed3a9f13a3b173d8a966075a
Scott <scottdouglas238@gmail.com>
2020-09-07 16:57:47 -0700

    Base Functionality Complete

--- server.js
+++ server.js
@@ -253,7 +253,7 @@ const removeRole = () => {
 }
 
 const viewAllEmployees = () => {
-   connection.query("SELECT employee.first_name, employee.last_name, role.title, role.salary, employee.manager_id FROM employee INNER JOIN role ON employee.role_id=role.id", function(err, res){
+   connection.query("SELECT employee.first_name, employee.last_name, role.title, department.departmentName, role.salary, employee.manager_id FROM employee INNER JOIN role ON employee.role_id=role.id INNER JOIN department ON role.department_id=department.id", function(err, res){
      if (err) throw err;
      console.log("------------------------------------------------------");
      console.table(res);
